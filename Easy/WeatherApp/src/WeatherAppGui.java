34bf54cf0caf28b2f4c7513f1f5cf372e4c8d726
belli <bellinna.uong@outlook.com>
2025-04-21 12:53:19 +0200

    App GUI

--- Easy/WeatherApp/src/WeatherAppGui.java
+++ Easy/WeatherApp/src/WeatherAppGui.java
@@ -24,16 +24,51 @@ public class WeatherAppGui extends JFrame {
         searchField.setFont(new Font("Dialog", Font.PLAIN,24));
         add(searchField);
 
-        JButton searchButton = new JButton(loadImage("src/assets/search.png"));
+        JButton searchButton = new JButton(loadImage("search.png"));
 
         searchButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         searchButton.setBounds(375,13,47,45);
         add(searchButton);
+
+        JLabel weatherConditionImage = new JLabel(loadImage("cloudy.png"));
+        weatherConditionImage.setBounds(0,125,450,217);
+        add(weatherConditionImage);
+
+        JLabel temperatureText = new JLabel("10°C");
+        temperatureText.setBounds(0,350,450,54);
+        temperatureText.setFont(new Font("Dialog", Font.BOLD,48));
+
+        temperatureText.setHorizontalAlignment(SwingConstants.CENTER);
+        add(temperatureText);
+
+        JLabel weatherConditionDesc = new JLabel("Cloudy");
+        weatherConditionDesc.setBounds(0,405,450,36);
+        weatherConditionDesc.setFont(new Font("Dialog", Font.PLAIN,37));
+        weatherConditionDesc.setHorizontalAlignment(SwingConstants.CENTER);
+        add(weatherConditionDesc);
+
+        JLabel humidityImage = new JLabel(loadImage("humidity.png"));
+        humidityImage.setBounds(15,500,74,66);
+        add(humidityImage);
+
+        JLabel humidityText = new JLabel("<html><b>Humidity</b> 100%<html>");
+        humidityText.setBounds(90,500,85,55);
+        humidityText.setFont(new Font("Dialog", Font.PLAIN,16));
+        add(humidityText);
+
+        JLabel windspeedImage = new JLabel(loadImage("windspeed.png"));
+        windspeedImage.setBounds(220,500,74,66);
+        add(windspeedImage);
+
+        JLabel windspeedText = new JLabel("<html><b>Windspeed</b> 15km<html>");
+        windspeedText.setBounds(310,500,85,55);
+        windspeedText.setFont(new Font("Dialog", Font.PLAIN,16));
+        add(windspeedText);
     }
 
     private ImageIcon loadImage(String resourcepath) {
         try{
-            BufferedImage image = ImageIO.read(new File(resourcepath));
+            BufferedImage image = ImageIO.read(getClass().getResource("/assets/" + resourcepath));
             return new ImageIcon(image);
         }catch(IOException e){
             e.printStackTrace();
